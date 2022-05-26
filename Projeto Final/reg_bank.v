9e60c29f8064a9653a7f9ddcb8ca8038f3e58728
YsabellaRomero <ysabellaromero5@gmail.com>
2022-05-26 18:32:48 +0100

    coisas boas

--- Projeto Final/reg_bank.v
+++ Projeto Final/reg_bank.v
@@ -25,31 +25,21 @@ module reg_bank(
 reg [31:0]  Real,
             Im;
 
-reg [31:0]  real_pos,                           // Read-only registers
-            real_zero,
-            real_neg,
-            im_pos,
-            im_zero,
-            im_neg;
-
 reg [0:63]  regs_bank [3:0];                    // 16 registers with a 64 bits dimension
-reg [0:63]  regs_const [8:0];                   // 9 sets of predefined constants
+wire [0:63]  regs_const [8:0];                  // 9 sets of predefined constants
 
-wire aux;
-            
-assign Real = inA[63:32];                       // Real part of register selwreg[i]
-assign Im = inA[31:0];                          // Imaginary part of register selwreg[i]
+reg aux;
 
 //Predefined constant sets
-assign regs_const[0] = {32'b01, 32'b01};                      // 1  + j
-assign regs_const[1] = {32'b00, 32'b01};                      // 0  + j
-assign regs_const[2] = {32'b11, 32'b01};                      // -1 + j
-assign regs_const[3] = {32'b01, 32'b00};                      // 1  + 0j
-assign regs_const[4] = {32'b00, 32'b00};                      // 0  + 0j
-assign regs_const[5] = {32'b11, 32'b00};                      // -1 + 0j
-assign regs_const[6] = {32'b01, 32'b11};                      // 1  - j
-assign regs_const[7] = {32'b00, 32'b11};                      // 0  - j
-assign regs_const[8] = {32'b11, 32'b11};                      // -1 - j
+assign regs_const[0] = {32'b01, 32'b01};        // 1  + j
+assign regs_const[1] = {32'b00, 32'b01};        // 0  + j
+assign regs_const[2] = {32'b11, 32'b01};        // -1 + j
+assign regs_const[3] = {32'b01, 32'b00};        // 1  + 0j
+assign regs_const[4] = {32'b00, 32'b00};        // 0  + 0j
+assign regs_const[5] = {32'b11, 32'b00};        // -1 + 0j
+assign regs_const[6] = {32'b01, 32'b11};        // 1  - j
+assign regs_const[7] = {32'b00, 32'b11};        // 0  - j
+assign regs_const[8] = {32'b11, 32'b11};        // -1 - j
 
 
 //------------------------------------------------------
@@ -57,9 +47,11 @@ assign regs_const[8] = {32'b11, 32'b11};                      // -1 - j
 always@(posedge clock)
 if ( reset )
 begin
-    Real <= 0;
-    Im <= 0;
-    regs_bank <= 0;
+    Real <= 32'b0;
+    Im <= 32'b0;
+    regs_bank[selwreg] <= 32'b0;
+    outA <= 64'b0;
+    outB <= 64'b0;
 end
 
 else
@@ -69,9 +61,13 @@ begin
     if( regwen )
 
     begin
+
+        Real <= inA[63:32];                       // Real part of register selwreg[i]
+        Im <= inA[31:0];                          // Imaginary part of register selwreg[i]
 	    case ( endwreg )
             2'b00: begin                                    // Write on both data fields
                 regs_bank[selwreg] <= {Real, Im};
+                //$display("lala = %d", regs_bank[selwreg]);
             end
 
             2'b10: begin                                    // Write on real part
@@ -94,28 +90,31 @@ begin
     begin
         if( enrregA )
         begin
-            if( cnstA )                                 
-                if( seloutA > 4'b1000)
+            if( cnstA ) 
+            begin                                
+                if( seloutA > 8)
                 begin
                     aux <= seloutA - 9;
-                    outA <= regs_const[aux];            // Load the output port A with the predefined constant
+                    outA <= regs_const[aux];                // Load the output port A with the predefined constant
                 end
-            
+            end
             else
-                outA <= regs_bank[seloutA];             // Load output port A with data from register
+                outA <= regs_bank[seloutA];                 // Load output port A with data from register
         end
 
         if( enrregB )
+        begin
             if( cnstB )
-                if( seloutB > 4'b1000)                  
+                if( seloutB > 8)                  
                 begin
                     aux <= seloutB - 9;
-                    outB <= regs_const[aux];            // Load the output port B with the predefined constant
+                    outB <= regs_const[aux];                // Load the output port B with the predefined constant
                 end
 
             else
-                outB <= regs_bank[seloutB];             // Load output port B with data from register
+                outB <= regs_bank[seloutB];                 // Load output port B with data from register
         end
+    end
 end
 
 
--- Projeto Final/testbench.v
+++ Projeto Final/testbench.v
@@ -1,18 +1,38 @@
-`timescale 1ns / 1ns
+`timescale 1ns / 1ps
 
 module testbench;
 
-reg  clock, reset;
-reg [31:0]      regA,
-                regB;
+initial
+begin
+  $dumpfile("mysimdata.vcd");// The filename with the waveform data
+  $dumpvars(0, testbench ); // The root node to dump end
+end
 
-wire [ 3:0]     opr; 
+// general parameters 
+parameter CLOCK_PERIOD = 10;              // Clock period in ns
+parameter MAX_SIM_TIME = 100_000_000;     // Set the maximum simulation time (time units=ns)
+
+reg  clock, reset, start, regwen;
+reg cnstA, cnstB, enrregA, enrregB;
+
+reg [63:0]  in;
+reg [ 3:0]  selwreg;
+reg [ 1:0]  endwreg;
+
+reg [ 3:0]  seloutA,
+            seloutB;
+
+wire [63:0] outA,
+            outB;
+
+//wire [ 3:0] opr; 
+
 
 reg_bank reg_bank_1(  
             .clock( clock ), 
             .reset( reset ), 
-            .regwe( regwe ),
-            .inA( inA ),
+            .regwen( regwen ),
+            .inA( in ),
             .selwreg( selwreg ),
             .endwreg( endwreg ),
             .outA( outA ),
@@ -26,15 +46,93 @@ reg_bank reg_bank_1(
 
 );
 
-//assign regA = outA;
-
-ALU_op ALU_op_1(
-                    .clock(clock),
-                    .reset(clock),
-                    .regA(regA),
-                    .regB(regB),
-                    .opr(opr),
-                    .regdest(regdest)
+/*
+ALUX ALUX_1(
+                    .clock(clock),              
+                    .reset(reset),             
+                    .inA(outA),            
+                    .inB(outB),                  
+                    .opr(opr),                   
+                    .start(start),          
+                    .outAB(result),                    
+                    .done(done)
  );
+*/
+
+
+
+//---------------------------------------------------
+// Setup initial signals
+initial
+begin
+  clock = 1'b0;
+  reset = 1'b0;
+  start = 1'b0;
+  in_aux = 64'b0;
+  regwen = 1'b0;
+  cnstA = 1'b0;
+  cnstB = 1'b0;
+  enrregA = 1'b0;
+  enrregB = 1'b0;
+end
+
+//---------------------------------------------------
+// generate a 50% duty-cycle clock signal
+initial
+begin  
+    forever
+        # (CLOCK_PERIOD / 2 ) clock = ~clock;
+end
+
+
+// Apply reset:
+initial
+begin
+	#10
+	reset = 0; 
+end
+
+//----------------------------------------------------
+//Test the inputs and outputs of the register bank
+integer i, in_aux;
+initial
+begin
+    //Declarar valores aleatorios para inA na mesma quantidade de registos
+    //Inserir um a um dos endereços de registos com diferentes valores 
+    //Verificar se no output os valores de cada um dos registos
+    for (i=0; i<16; i=i+1)
+    begin
+        #10
+        endwreg = 2'b00;
+        enrregA = 1'b1;
+        enrregB = 1'b1;    
+        cnstA = 1'b0;
+        cnstB = 1'b0;  
+        in_aux = i*400;
+        execbr(in_aux, i);
+        #10
+        $display("Expected Value: %d || Obtained: %d", in_aux, outA);
+    end
+    $stop;
+
+end
+
+task execbr;
+input [63:0] inA;
+input i;
+begin
+  in = inA;   // Apply operands
+  @(posedge clock);
+  regwen = 1'b1;
+  selwreg = i;
+  seloutA = i;
+  seloutB = i;
+  #10
+  @(negedge clock);
+  regwen = 1'b0;                
+end  
+endtask
+
 
 endmodule
+
