19bc43b290ed4bf3239dc9f2f7adaa6fc7441e9e
tuliosbhz <tulio.soares.bhz@gmail.com>
2022-06-07 17:17:05 +0100

    Machine State to ALUX, controller to RAM

--- Projeto Final/alux.v
+++ Projeto Final/alux.v
@@ -10,10 +10,10 @@ module ALUX(
                     output done
  );
 
-reg [31:0]   Real_A, 
-                    Real_B,
-                    Im_A,
-                    Im_B;
+reg [31:0]  Real_A, 
+            Real_B,
+            Im_A,
+            Im_B;
 
 reg         sum_sub,
             complex_real,
@@ -64,6 +64,13 @@ mod mod_1(
                     .done( done ),
                     .out( out_mod )
 );
+
+controller controller_1 (
+                    .clock( clock ),
+                    .reset( reset ),
+                    .maxclock( )
+
+);
 //-------------------------------------------------------
 
 always@(posedge clock)
@@ -85,65 +92,288 @@ begin
     Real_B <= inB[63:32];                       // Real part of data input B
     Im_B <= inB[31:0];                          // Imaginary part of data input B
 
-    if ( start )
     begin
-        case ( opr )
-        4'b0000: begin                                                 // A
+	case ( state )
+	4'b0000: 
+	begin
+		if( start )
+		begin
+			case( opr )
+			4'b0000: state <= 4'b0001;
+			4'b0001: state <= 4'b0010;
+			4'b0010: state <= 4'b0011;
+			4'b0011: state <= 4'b0100;
+			4'b0100: state <= 4'b0101;
+			4'b0110: state <= 4'b0110;
+			4'b1000: state <= 4'b0111;
+			4'b1001: state <= 4'b1000;
+			4'b1010: state <= 4'b1001;
+			endcase
+		end 
+	end
+	4'b0001:                                                     // A
+	begin 
+		if ( done || reset ) 
+		begin
+			done <= 0;
+			counter <= 0;
+			reset <= 0;
+			state <= 4'b0000;
+		end
+
+		if ( !alux_done )
+		begin
+            maxclock <= 1;
             out <= inA;
-        end
+			if( counter < maxclock )
+				counter <= counter + 1;
+			else
+			begin
+				done <= 1;
+				state <= 4'b0000;
+			end
+		end
+	end
+		
+	4'b0010: begin 
+		if ( alux_done || done || reset ) 
+		begin
+			done <= 0;
+			counter <= 0;
+			reset <= 0;
+			state <= 4'b0000;
+		end
 
-        4'b0001: begin                                                 // B
-            out <= inB;
-        end
+		if ( !alux_done )
+		begin
+			if( counter < maxclock )
+				counter <= counter + 1;
+			begin
+				done <= 1;
+				state <= 4'b0000;
+			end
+		end
+	end
+		
+	4'b0011: 
+	begin
+			if ( alux_done || done || reset ) 
+		begin
+			done <= 0;
+			counter <= 0;
+			reset <= 0;
+			state <= 4'b0000;
+		end
 
-        4'b0010: begin                                                 // A + B
-            sum_sub <= 1;
-            out <= out_add_sub;
-        end
+		if ( !alux_done )
+		begin
+			if( counter < maxclock )
+				counter <= counter + 1;
+			begin
+				done <= 1;
+				state <= 4'b0000;
+			end
+		end
+	end
 
-        4'b0011: begin                                                 // A - B
-            sum_sub <= 0;
-            out <= out_add_sub;
-        end
+	4'b0100: 
+	begin
+		if ( alux_done || done || reset ) 
+		begin
+			done <= 0;
+			counter <= 0;
+			reset <= 0;
+			state <= 4'b0000;
+		end
 
-        4'b0100: begin                                                 // A * B
-            complex_real <= 1;
-            out <= out_mult;
-        end
+		if ( !alux_done )
+		begin
+			if( counter < maxclock )
+				counter <= counter + 1;
+			begin
+				done <= 1;
+				state <= 4'b0000;
+			end
+		end
+	end
 
-        4'b0110: begin                                                 // RE(A) * RE(B), IM(A) * IM(B)
-            complex_real <= 0;
-            out <= out_mult;
-        end
+	4'b0101: 
+	begin
+		if ( alux_done || done || reset ) 
+		begin
+			done <= 0;
+			counter <= 0;
+			reset <= 0;
+			state <= 4'b0000;
+		end
 
-        4'b1000: begin                                                 // A == B
-            if( Real_A == Real_B && Im_A == Im_B )
-                out <= 64'b01;
-            else
-                out <= 0;
-        end
+		if ( !alux_done )
+		begin
+			if( counter < maxclock )
+				counter <= counter + 1;
+			begin
+				done <= 1;
+				state <= 4'b0000;
+			end
+		end
+	end
+
+	4'b0110: 
+	begin
+		if ( alux_done || done || reset ) 
+		begin
+			done <= 0;
+			counter <= 0;
+			reset <= 0;
+			state <= 4'b0000;
+		end
+
+		if ( !alux_done )
+		begin
+			if( counter < maxclock )
+				counter <= counter + 1;
+			begin
+				done <= 1;
+				state <= 4'b0000;
+			end
+		end
+	end
+
+	4'b0111: 
+	begin
+		if ( alux_done || done || reset ) 
+		begin
+			done <= 0;
+			counter <= 0;
+			reset <= 0;
+			state <= 4'b0000;
+		end
+
+		if ( !alux_done )
+		begin
+			if( counter < maxclock )
+				counter <= counter + 1;
+			begin
+				done <= 1;
+				state <= 4'b0000;
+			end
+		end
+	end
+
+	4'b1000: 
+	begin
+		if ( alux_done || done || reset ) 
+		begin
+			done <= 0;
+			counter <= 0;
+			reset <= 0;
+			state <= 4'b0000;
+		end
+
+		if ( !alux_done )
+		begin
+			if( counter < maxclock )
+				counter <= counter + 1;
+			begin
+				done <= 1;
+				state <= 4'b0000;
+			end
+		end
+	end
+
+	4'b1001: 
+	begin
+		if ( alux_done || done || reset ) 
+		begin
+			done <= 0;
+			counter <= 0;
+			reset <= 0;
+			state <= 4'b0000;
+		end
+		
+		if ( !alux_done )
+		begin
+			if( counter < maxclock )
+				counter <= counter + 1;
+			begin
+				done <= 1;
+				state <= 4'b0000;
+			end
+		end
+	end
+
+	default: state<= 4'b0000;
+	endcase
+end
+
+    // if ( start )
+    // begin
+    //     case ( opr )
+    //     4'b0000: begin                                                 // A
+    //         maxclock <= 1;
+    //         out <= inA;
+    //     end
+
+    //     4'b0001: begin                                                 // B
+    //         maxclock <= 1;
+    //         out <= inB;
+    //     end
+
+    //     4'b0010: begin                                                 // A + B
+    //         maxclock <= 2;
+    //         sum_sub <= 1;
+    //         out <= out_add_sub;
+    //     end
+
+    //     4'b0011: begin                                                 // A - B
+    //         maxclock <= 2;
+    //         sum_sub <= 0;
+    //         out <= out_add_sub;
+    //     end
+
+    //     4'b0100: begin                                                 // A * B
+    //         maxclock <= 6;
+    //         complex_real <= 1;
+    //         out <= out_mult;
+    //     end
+
+    //     4'b0110: begin                                                 // RE(A) * RE(B), IM(A) * IM(B)
+    //         maxclock <= 4;
+    //         complex_real <= 0;
+    //         out <= out_mult;
+    //     end
+
+    //     4'b1000: begin                                                 // A == B
+    //         maxclock <= 1;
+    //         if( Real_A == Real_B && Im_A == Im_B )
+    //             out <= 64'b01;
+    //         else
+    //             out <= 0;
+    //     end
         
-        4'b1001: begin                                                 // { MOD(A), ANG(A) }
-            mod_A_B <= 1;
-            out <= out_mod;
-        end
+    //     4'b1001: begin                                                 // { MOD(A), ANG(A) }
+    //         maxclock <= 38;
+    //         mod_A_B <= 1;
+    //         out <= out_mod;
+    //     end
 
-        4'b1010: begin                                                 // { MOD(B), ANG(B) }
-            mod_A_B <= 0;
-            out <= out_mod;
-        end
+    //     4'b1010: begin                                                 // { MOD(B), ANG(B) }
+    //         maxclock <= 38;
+    //         mod_A_B <= 0;
+    //         out <= out_mod;
+    //     end
         
-        default: begin
-            out <= 64'b0;
-        end
+    //     default: begin
+    //         out <= 64'b0;
+    //     end
 
-        endcase
+    //     endcase
 
-        if( done )
-        begin
-            outAB <= out;
-        end 
-    end
+    //     if( done )
+    //     begin
+    //         outAB <= out;
+    //     end 
+    // end
 end
 
  endmodule
--- Projeto Final/controller.v
+++ Projeto Final/controller.v
@@ -3,6 +3,7 @@ module controller(
 				 input reset,
 				 input maxclock,
 				 input opr,
+				 input alux_done,
 				 output done
 				);
 
@@ -10,208 +11,228 @@ reg [3:0] state;
 reg counter;	
 
 always @(posedge clock)
-begin
-	case ( state )
-	4'b0000: begin
-		if( start )
-		begin
-			case( opr )
-			4'b0000: state <= 4'b0001;
-			4'b0001: state <= 4'b0010;
-			4'b0010: state <= 4'b0011;
-			4'b0011: state <= 4'b0100;
-			4'b0100: state <= 4'b0101;
-			4'b0110: state <= 4'b0110;
-			4'b1000: state <= 4'b0111;
-			4'b1001: state <= 4'b1000;
-			4'b1010: state <= 4'b1001;
-			endcase
-		end 
-	end
-	4'b0001: begin 
-		if ( done || reset ) 
-		begin
-			done <= 0;
-			counter <= 0;
-			reset <= 0;
-			state <= 4'b0000;
-		end
 
-		if ( !done )
-		begin
-			if( counter != 1 )
-				counter <= counter + 1;
-			else
-			begin
-				done <= 1;
-				state <= 4'b0000;
-			end
-		end
-	end
-		
-	4'b0010: begin 
-		if ( done || reset ) 
-		begin
-			done <= 0;
-			counter <= 0;
-			reset <= 0;
-			state <= 4'b0000;
-		end
-
-		if ( !done )
-		begin
-			if( counter != 1 )
-				counter <= counter + 1;
-			begin
-				done <= 1;
-				state <= 4'b0000;
-			end
-		end
-	end
-		
-	4'b0011: begin
-			if ( done || reset ) 
-		begin
-			done <= 0;
-			counter <= 0;
-			reset <= 0;
-			state <= 4'b0000;
-		end
-
-		if ( !done )
-		begin
-			if( counter != 2 )
-				counter <= counter + 1;
-			begin
-				done <= 1;
-				state <= 4'b0000;
-			end
-		end
-	end
-
-	4'b0100: begin
-		if ( done || reset ) 
-		begin
-			done <= 0;
-			counter <= 0;
-			reset <= 0;
-			state <= 4'b0000;
-		end
-
-		if ( !done )
-		begin
-			if( counter != 2 )
-				counter <= counter + 1;
-			begin
-				done <= 1;
-				state <= 4'b0000;
-			end
-		end
-	end
-
-	4'b0101: begin
-		if ( done || reset ) 
-		begin
-			done <= 0;
-			counter <= 0;
-			reset <= 0;
-			state <= 4'b0000;
-		end
-
-		if ( !done )
-		begin
-			if( counter != 6 )
-				counter <= counter + 1;
-			begin
-				done <= 1;
-				state <= 4'b0000;
-			end
-		end
-	end
-
-	4'b0110: begin
-		if ( done || reset ) 
-		begin
-			done <= 0;
-			counter <= 0;
-			reset <= 0;
-			state <= 4'b0000;
-		end
-
-		if ( !done )
-		begin
-			if( counter != 4 )
-				counter <= counter + 1;
-			begin
-				done <= 1;
-				state <= 4'b0000;
-			end
-		end
-	end
-
-	4'b0111: begin
-		if ( done || reset ) 
-		begin
-			done <= 0;
-			counter <= 0;
-			reset <= 0;
-			state <= 4'b0000;
-		end
-
-		if ( !done )
-		begin
-			if( counter != 1 )
-				counter <= counter + 1;
-			begin
-				done <= 1;
-				state <= 4'b0000;
-			end
-		end
-	end
-
-	4'b1000: begin
-		if ( done || reset ) 
-		begin
-			done <= 0;
-			counter <= 0;
-			reset <= 0;
-			state <= 4'b0000;
-		end
-
-		if ( !done )
-		begin
-			if( counter != 38 )
-				counter <= counter + 1;
-			begin
-				done <= 1;
-				state <= 4'b0000;
-			end
-		end
-	end
-
-	4'b1001: begin
-		if ( done || reset ) 
-		begin
-			done <= 0;
-			counter <= 0;
-			reset <= 0;
-			state <= 4'b0000;
-		end
-		
-		if ( !done )
-		begin
-			if( counter != 38 )
-				counter <= counter + 1;
-			begin
-				done <= 1;
-				state <= 4'b0000;
-			end
-		end
-	end
-
-	default: state<= 4'b0000;
-	endcase
+if( !alux_done )
+begin 
+ 	//Wait to start the next operation
 end
+else 
+begin
+	//Register in RAM the result of the last operation
+	//Start the next operation from RAM
+end
+
+// begin
+// 	case ( state )
+// 	4'b0000: 
+// 	begin
+// 		if( start )
+// 		begin
+// 			case( opr )
+// 			4'b0000: state <= 4'b0001;
+// 			4'b0001: state <= 4'b0010;
+// 			4'b0010: state <= 4'b0011;
+// 			4'b0011: state <= 4'b0100;
+// 			4'b0100: state <= 4'b0101;
+// 			4'b0110: state <= 4'b0110;
+// 			4'b1000: state <= 4'b0111;
+// 			4'b1001: state <= 4'b1000;
+// 			4'b1010: state <= 4'b1001;
+// 			endcase
+// 		end 
+// 	end
+// 	4'b0001: 
+// 	begin 
+// 		if ( alux_done || done || reset ) 
+// 		begin
+// 			done <= 0;
+// 			counter <= 0;
+// 			reset <= 0;
+// 			state <= 4'b0000;
+// 		end
+
+// 		if ( !alux_done )
+// 		begin
+// 			if( counter < maxclock )
+// 				counter <= counter + 1;
+// 			else
+// 			begin
+// 				done <= 1;
+// 				state <= 4'b0000;
+// 			end
+// 		end
+// 	end
+		
+// 	4'b0010: begin 
+// 		if ( alux_done || done || reset ) 
+// 		begin
+// 			done <= 0;
+// 			counter <= 0;
+// 			reset <= 0;
+// 			state <= 4'b0000;
+// 		end
+
+// 		if ( !alux_done )
+// 		begin
+// 			if( counter < maxclock )
+// 				counter <= counter + 1;
+// 			begin
+// 				done <= 1;
+// 				state <= 4'b0000;
+// 			end
+// 		end
+// 	end
+		
+// 	4'b0011: 
+// 	begin
+// 			if ( alux_done || done || reset ) 
+// 		begin
+// 			done <= 0;
+// 			counter <= 0;
+// 			reset <= 0;
+// 			state <= 4'b0000;
+// 		end
+
+// 		if ( !alux_done )
+// 		begin
+// 			if( counter < maxclock )
+// 				counter <= counter + 1;
+// 			begin
+// 				done <= 1;
+// 				state <= 4'b0000;
+// 			end
+// 		end
+// 	end
+
+// 	4'b0100: 
+// 	begin
+// 		if ( alux_done || done || reset ) 
+// 		begin
+// 			done <= 0;
+// 			counter <= 0;
+// 			reset <= 0;
+// 			state <= 4'b0000;
+// 		end
+
+// 		if ( !alux_done )
+// 		begin
+// 			if( counter < maxclock )
+// 				counter <= counter + 1;
+// 			begin
+// 				done <= 1;
+// 				state <= 4'b0000;
+// 			end
+// 		end
+// 	end
+
+// 	4'b0101: 
+// 	begin
+// 		if ( alux_done || done || reset ) 
+// 		begin
+// 			done <= 0;
+// 			counter <= 0;
+// 			reset <= 0;
+// 			state <= 4'b0000;
+// 		end
+
+// 		if ( !alux_done )
+// 		begin
+// 			if( counter < maxclock )
+// 				counter <= counter + 1;
+// 			begin
+// 				done <= 1;
+// 				state <= 4'b0000;
+// 			end
+// 		end
+// 	end
+
+// 	4'b0110: 
+// 	begin
+// 		if ( alux_done || done || reset ) 
+// 		begin
+// 			done <= 0;
+// 			counter <= 0;
+// 			reset <= 0;
+// 			state <= 4'b0000;
+// 		end
+
+// 		if ( !alux_done )
+// 		begin
+// 			if( counter < maxclock )
+// 				counter <= counter + 1;
+// 			begin
+// 				done <= 1;
+// 				state <= 4'b0000;
+// 			end
+// 		end
+// 	end
+
+// 	4'b0111: 
+// 	begin
+// 		if ( alux_done || done || reset ) 
+// 		begin
+// 			done <= 0;
+// 			counter <= 0;
+// 			reset <= 0;
+// 			state <= 4'b0000;
+// 		end
+
+// 		if ( !alux_done )
+// 		begin
+// 			if( counter < maxclock )
+// 				counter <= counter + 1;
+// 			begin
+// 				done <= 1;
+// 				state <= 4'b0000;
+// 			end
+// 		end
+// 	end
+
+// 	4'b1000: 
+// 	begin
+// 		if ( alux_done || done || reset ) 
+// 		begin
+// 			done <= 0;
+// 			counter <= 0;
+// 			reset <= 0;
+// 			state <= 4'b0000;
+// 		end
+
+// 		if ( !alux_done )
+// 		begin
+// 			if( counter < maxclock )
+// 				counter <= counter + 1;
+// 			begin
+// 				done <= 1;
+// 				state <= 4'b0000;
+// 			end
+// 		end
+// 	end
+
+// 	4'b1001: 
+// 	begin
+// 		if ( alux_done || done || reset ) 
+// 		begin
+// 			done <= 0;
+// 			counter <= 0;
+// 			reset <= 0;
+// 			state <= 4'b0000;
+// 		end
+		
+// 		if ( !alux_done )
+// 		begin
+// 			if( counter < maxclock )
+// 				counter <= counter + 1;
+// 			begin
+// 				done <= 1;
+// 				state <= 4'b0000;
+// 			end
+// 		end
+// 	end
+
+// 	default: state<= 4'b0000;
+// 	endcase
+// end
 	
 
 					
